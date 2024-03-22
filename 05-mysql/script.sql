592c3e83fb2c229bf5f170d92a19152f45b47ad9
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-03-21 21:23:56 -0300

    21-03

--- 05-mysql/script.sql
+++ 05-mysql/script.sql
@@ -3,7 +3,7 @@ Create database nodemysql;
 use nodemysql;
 
 create table books (
-	id int not null,
+	id int not null auto_increment,
     title varchar (255) null,
     pageqty int null,
     primary key(id)
